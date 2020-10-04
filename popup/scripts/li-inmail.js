dcfde63f53a24dd6e0eb8c58f180ba2fbad7fa53
Nik0gda <cebanu.dan3@gmail.com>
2020-10-05 00:32:38 +0300

    Li-inmail feature

---
+++ popup/scripts/li-inmail.js
@@ -0,0 +1,230 @@
+window.addEventListener("load", (event) => {
+  document.getElementById("saveSettings").addEventListener("click", save);
+  document.getElementById("startSpamming").addEventListener("click", startSpam);
+  document
+    .getElementById("resetCounter")
+    .addEventListener("click", resetCounter);
+  document.getElementById("fileUpload").onchange = showFile;
+  var snackbar = document.querySelector("#snackbar");
+  checkForUpdate();
+  load();
+});
+
+const showFile = async (e) => {
+  const input = e.target;
+  let file = input.files[0];
+  console.log(file, input, e);
+  let content = await file.text();
+  console.log($);
+  content = $.csv.toArrays(content);
+  const indexes = [];
+  const indexesToFind = [
+    "sn_hash_id",
+    "first_name",
+    "last_name",
+    "current_company",
+  ];
+  const parsed = {};
+  let accumulator = 0;
+  for (name of indexesToFind) {
+    indexes[accumulator] = content[0].indexOf(name);
+    parsed[name] = [];
+    accumulator++;
+  }
+  for (let i = 1; i < content.length; i++) {
+    for (y in indexesToFind) {
+      array = parsed[indexesToFind[y]];
+      if (y == 0) {
+        array.push(content[i][indexes[y]].split(",")[0]);
+        continue;
+      }
+      array.push(content[i][indexes[y]]);
+    }
+  }
+  chrome.storage.local.set({ liSpamProfiles: parsed }, function (err) {
+    if (err) {
+      snackbarSettings["message"] = err.message;
+    } else {
+      snackbarSettings["message"] =
+        "Profiles for LI. Inmail Spam successfully saved";
+    }
+    snackbar.MaterialSnackbar.showSnackbar(snackbarSettings);
+  });
+};
+
+const checkForUpdate = async () => {
+  const downloadTime = getDownloadTime();
+  const request = await fetch(
+    "https://api.github.com/repos/Nik0gda/liext/commits/master"
+  );
+  const commitTime = new Date(
+    Date.parse((await request.json()).commit.committer.date)
+  );
+  if (downloadTime < commitTime) {
+    const snackbarSettings = {
+      message: "New version available",
+      timeout: 60 * 60 * 1000,
+    };
+    snackbar.MaterialSnackbar.showSnackbar(snackbarSettings);
+  }
+};
+
+const save = () => {
+  let saveError = "";
+  const ids = [
+    "nextUser",
+    "clickMessage",
+    "insertTitle",
+    "insertMessage",
+    "clickSend",
+    "closeWindow",
+  ];
+  const settings = {};
+  const snackbarSettings = { message: "", timeout: 2000 };
+  for (const id of ids) {
+    const text = document.getElementById(id).value.split("-");
+    if (isNaN(parseInt(text[0])) || isNaN(parseInt(text[1]))) {
+      saveError = `${id} field must contain only numbers and dashes( - )!`;
+    }
+    settings[id] = { min: text[0], max: text[1] };
+  }
+  if (document.getElementById("switch").classList.contains("is-checked")) {
+    settings["other"] = true;
+  } else {
+    settings["other"] = false;
+  }
+  settings["message"] = document.getElementById("message").value;
+  settings["subject"] = document.getElementById("subject").value;
+  settings["limit"] = document.getElementById("limit").value;
+  if (saveError) {
+    snackbarSettings["message"] = saveError;
+    snackbar.MaterialSnackbar.showSnackbar(snackbarSettings);
+    return;
+  }
+  chrome.storage.local.set({ settings: settings }, function (err) {
+    if (err) {
+      snackbarSettings["message"] = err.message;
+    } else {
+      snackbarSettings["message"] = "Settings were successfully saved";
+    }
+    snackbar.MaterialSnackbar.showSnackbar(snackbarSettings);
+  });
+};
+
+const resetCounter = () => {
+  chrome.storage.local.set({ counter: 0 }, (err) => {
+    const snackbarSettings = { message: "Counter resetted", timeout: 2000 };
+    document.getElementById("counter").innerHTML = `Profiles Proccessed: 0`;
+    if (err) {
+      snackbarSettings.message = err.message;
+    }
+    snackbar.MaterialSnackbar.showSnackbar(snackbarSettings);
+  });
+};
+const load = () => {
+  const ids = [
+    "nextUser",
+    "clickMessage",
+    "insertTitle",
+    "insertMessage",
+    "clickSend",
+    "closeWindow",
+  ];
+  chrome.storage.local.get(["settings"], (obj) => {
+    if (!obj) {
+      return;
+    }
+    obj = obj["settings"];
+
+    for (id of ["limit", "message", "subject"]) {
+      if (obj[id]) {
+        document.getElementById(id).value = obj[id];
+        document.getElementById(id).parentElement.classList.add("is-dirty");
+      }
+    }
+    if (obj.other) {
+      document.getElementById("switch").classList.add("is-checked");
+    }
+    for (const id of ids) {
+      if (obj[id]) {
+        document.getElementById(id).parentElement.classList.add("is-dirty");
+        document.getElementById(
+          id
+        ).value = `${obj[id]["min"]}-${obj[id]["max"]}`;
+      }
+    }
+    document.getElementById(id).parentElement.classList.add("is-dirty");
+
+    chrome.storage.local.get(["spam"], (obj) => {
+      if (!obj) {
+        chrome.storage.local.set({ spam: false });
+      } else {
+        if (obj.spam == true) {
+          document
+            .getElementById("startSpamming")
+            .classList.remove("mdl-button--colored");
+          document.getElementById("startSpamming").innerHTML = "Stop Spamming";
+        } else {
+          document
+            .getElementById("startSpamming")
+            .classList.add("mdl-button--colored");
+          document.getElementById("startSpamming").innerHTML = "Start Spamming";
+        }
+      }
+    });
+  });
+  chrome.storage.local.get(["counter"], (obj) => {
+    if (!obj["counter"]) {
+      chrome.storage.local.set({ counter: 0 });
+      obj["counter"] = 0;
+    }
+    document.getElementById(
+      "counter"
+    ).innerHTML = `Profiles Proccessed: ${obj["counter"]}`;
+  });
+};
+
+const startSpam = () => {
+  chrome.storage.local.get(["spam"], (obj) => {
+    if (!obj) {
+      chrome.storage.local.set({ spam: false }, () => {
+        obj = false;
+      });
+    } else {
+      obj = obj.spam;
+    }
+    if (!obj) {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { command: "spam" }, (response) => {
+          if (response.result == "Started Spamming") {
+            document
+              .getElementById("startSpamming")
+              .classList.remove("mdl-button--colored");
+            document.getElementById("startSpamming").innerHTML =
+              "Stop Spamming";
+          }
+        });
+      });
+    } else {
+      chrome.storage.local.set({ spam: false });
+      document
+        .getElementById("startSpamming")
+        .classList.add("mdl-button--colored");
+      document.getElementById("startSpamming").innerHTML = "Start Spamming";
+    }
+  });
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const { command } = request;
+  if (command === "spammed") {
+    chrome.storage.local.set({
+      spam: false,
+    });
+    document
+      .getElementById("startSpamming")
+      .classList.add("mdl-button--colored");
+    document.getElementById("startSpamming").innerHTML = "Start Spamming";
+    return;
+  }
+});
