6bd55746adbc517fa226ecf8563ed7b102942147
Nik0gda <cebanu.dan3@gmail.com>
2020-10-21 16:11:28 +0300

    Msg btn fix

--- content.js
+++ content.js
@@ -44,8 +44,14 @@ const spamLi = async ({ first_name, last_name, company_name }) => {
     const topCard = document.querySelector("section.pv-top-card");
     try {
       const btn = topCard.getElementsByClassName("message-anywhere-button")[0];
-      if (!btn.parentElement.parentElement.tagName.toLocaleLowerCase() == "li")
+      let clicked = false;
+      if (
+        !btn.parentElement.parentElement.tagName.toLocaleLowerCase() == "li"
+      ) {
         btn.click();
+        clicked = true;
+      }
+      if (!clicked) throw "Didn't click";
     } catch (err) {
       const liel = topCard
         .getElementsByClassName("artdeco-dropdown__content")[0]
